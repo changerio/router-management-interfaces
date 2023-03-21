3a24cb46bab94e1018a4948a2eb716564fa36a0e
Carl Park <dkdkajej@gmail.com>
2023-03-21 11:26:13 +0900

    next: add base path config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "https://changerio.github.io/router-management-interfaces",
 };
 
 module.exports = nextConfig;
